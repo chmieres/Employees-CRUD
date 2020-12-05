55a8ff1e55703df348f8686690a52ae864679627
Christian Mieres <chmieres6@gmail.com>
2020-12-05 18:43:52 -0500

    Simple crud program node
    Simple crud program node

---
+++ Employees/models/db.js
@@ -0,0 +1,13 @@
+//db.js inside the models folder!
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/EmployeeDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology:true,
+  useFindAndModify: false }, (err) => {
+    if(!err) {
+        console.log('MongoDB connection successful');
+    } else {
+        console.log('connection failed', err);
+    }
+} );
+require('./employee.model');
